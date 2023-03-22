9244ea654d1bde581106f20c06d908793b69fff4
euclides <euclideskinto@gmail.com>
2023-03-22 09:37:34 -0300

    .

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -16,25 +16,22 @@ class RegisteredUserController extends Controller
     /**
      * Handle an incoming registration request.
      *
-     * @throws \Illuminate\Validation\ValidationException
+//     * @throws \Illuminate\Validation\ValidationException
      */
     public function store(Request $request): Response
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
 
-        $user = User::create([
+        $user = User::query()->create([
             'name' => $request->name,
             'email' => $request->email,
+            'is_admin' => false,
             'password' => Hash::make($request->password),
         ]);
 
-        event(new Registered($user));
-
-        Auth::login($user);
+//        dd($user);
+//        event(new Registered($user));
+//
+//        Auth::login($user);
 
         return response()->noContent();
     }
